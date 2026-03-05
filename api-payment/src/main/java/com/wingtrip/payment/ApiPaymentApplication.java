acf109b3ef18d8a17a722bcbf0d6acaa1913d5b7
Erimtz <erimtz2@gmail.com>
2026-03-05 16:09:00 -0500

    feat(api-payment): add main application class

--- api-payment/src/main/java/com/wingtrip/payment/ApiPaymentApplication.java
+++ api-payment/src/main/java/com/wingtrip/payment/ApiPaymentApplication.java
@@ -1,7 +1,15 @@
 package com.wingtrip.payment;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = "com.wingtrip.payment")
 public class ApiPaymentApplication {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(ApiPaymentApplication.class, args);
     }
 }
